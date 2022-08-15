9209fdb12f9b6ba090adcc202e85ba2bb5a0226f
damipoc <damian7DP@hotmail.com>
2022-08-15 13:44:58 +0100

    PET-1, PET-2, PET-3, PET-4, PET-5, PET-18

---
+++ src/main/java/com/qa/main/services/AnimalService.java
@@ -0,0 +1,52 @@
+package com.qa.main.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.main.domain.Animal;
+import com.qa.main.repos.AnimalRepo;
+
+@Service
+public class AnimalService {
+
+    private AnimalRepo repo;
+
+    public AnimalService(AnimalRepo repo) {
+        this.repo = repo;
+    }
+
+    public Animal create(Animal animal) {
+
+        return repo.saveAndFlush(animal);
+    }
+
+    public List<Animal> getAll() {
+
+        return repo.findAll();
+    }
+
+    public Animal getByID(long id) {
+
+        return repo.findById(id).get();
+    }
+
+    public Animal updateAnimal(long id, Animal animal) {
+        Animal existing = repo.findById(id).get();
+
+        existing.setType(animal.getType());
+        existing.setName(animal.getName());
+        existing.setAge(animal.getAge());
+        existing.setNotes(animal.getNotes());
+
+        return repo.saveAndFlush(existing);
+    }
+    
+    public Boolean removeAnimal(long id) {
+
+        repo.deleteById(id);
+
+        return !repo.existsById(id);
+    }
+
+}
