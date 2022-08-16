30cb50c5c826a6313c51f4767898f571bb52835c
damipoc <damian7DP@hotmail.com>
2022-08-16 14:45:49 +0100

    PET-14, PET-15

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,25 +5,49 @@
 //Divs
 let resultsDiv = document.querySelector("#results-div");
 
+//Forms
+let formPopup = document.querySelector("#formPopup");
+
 //Inputs
 let typeInput = document.querySelector("#ageInput");
 let nameInput = document.querySelector("#nameInput");
 let ageInput = document.querySelector("#ageInput");
 let notesInput = document.querySelector("#notesInput");
 
+let typeUpdate = document.querySelector("#typeUpdate");
+let nameUpdate = document.querySelector("#nameUpdate");
+let ageUpdate = document.querySelector("#ageUpdate");
+let notesUpdate = document.querySelector("#notesUpdate");
+
+
 //Button
 let createBtn = document.querySelector("#createBtn");
-
 
 // Functions
 let printResults = (result) => {
     let entryDiv = document.createElement("div");
-    entryDiv.setAttribute("class", "entry-div")
-    entryDiv.textContent = `${result.id} | ${result.type} | ${result.name} | ${result.age} | ${result.notes}`
+    let updateBtn = document.createElement("button");
+    let deleteBtn = document.createElement("button");
 
+    entryDiv.setAttribute("class", "entry-div");
+
+    updateBtn.setAttribute("class", "btn btn-warning btn-sm");
+    updateBtn.setAttribute("id", "updateBtn");
+    updateBtn.setAttribute("onClick", `formUpdate(${result.id})`);
+
+
+    deleteBtn.setAttribute("class", "btn btn-danger btn-sm");
+    deleteBtn.setAttribute("id", "deleteBtn");
+    deleteBtn.setAttribute("onClick", `deleteEntry(${result.id})`);
+
+    entryDiv.textContent = `${result.id} | ${result.type} | ${result.name} | ${result.age} | ${result.notes}`;
+    updateBtn.textContent = "Update";
+    deleteBtn.textContent = "Delete";
+
+    entryDiv.appendChild(updateBtn);
+    entryDiv.appendChild(deleteBtn);
     resultsDiv.appendChild(entryDiv);
 }
-
 
 let getAll = () => {
     axios.get("http://localhost:8080/animal/getAll")
@@ -48,16 +72,64 @@ let create = () => {
         "notes": notesInput.value
     }
 
-    console.log(obj);
     axios.post("http://localhost:8080/animal/create", obj)
         .then((res) => {
             getAll();
         }).catch((err => { console.log(err); }));
 
+}
+
+let formUpdate = (id) => {
+    div_show();
+
+    let confirmBtn = document.createElement("button");
+
+    confirmBtn.setAttribute("class", "btn btn-warning btn-sm");
+    confirmBtn.setAttribute("id", "temp");
+    confirmBtn.setAttribute("onClick", `confirmUpdate(${id})`);
+
+    confirmBtn.textContent = "Confirm";
+    formPopup.appendChild(confirmBtn);
+
+}
+
+let confirmUpdate = (id) => {
 
 
+    let obj = {
+        "type": typeUpdate.value,
+        "name": nameUpdate.value,
+        "age": ageUpdate.value,
+        "notes": notesUpdate.value
+    }
+
+
+    axios.put(`http://localhost:8080/animal/update/${id}`, obj)
+        .then((resp) => {
+            getAll();
+        }).catch((err) => { console.error(err); });
+
+    div_hide();
+
+}
+
+let deleteEntry = (id) => {
+
+    axios.delete(`http://localhost:8080/animal/delete/${id}`)
+        .then((res) => {
+            getAll();
+        }).catch((err => { console.log(err); }));
+}
+
+let div_show = () => {
+    document.getElementById('abc').style.display = "block";
+}
+
+let div_hide = () => {
+    formPopup.removeChild(document.getElementById("temp"));
+    document.getElementById('abc').style.display = "none";
 }
 
 
 // Listeners
-createBtn.addEventListener("click", create)
+createBtn.addEventListener("click", create);
