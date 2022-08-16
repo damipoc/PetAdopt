d1979045aa20b91280f26ad39c1956e4b3dbec79
damipoc <damian7DP@hotmail.com>
2022-08-16 11:49:37 +0100

    PET-11, PET-12, PET-13

--- src/main/java/com/qa/main/controllers/AnimalController.java
+++ src/main/java/com/qa/main/controllers/AnimalController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.qa.main.domain.Animal;
 import com.qa.main.services.AnimalService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/animal")
 public class AnimalController {
     
